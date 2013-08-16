2bc60c9a1c82633c34078104d115437b8bc93da1
George S. Mendoza <gsmendoza@gmail.com>
2013-08-16 18:27:19 +0800

    I should be able to add an itinerary.

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -6,4 +6,14 @@ class ItinerariesController < ApplicationController
   def index
     @itineraries = Itinerary.all
   end
+  
+  def new
+  end
+  
+  def create
+    @itinerary = Itinerary.new
+    @itinerary.attributes = params[:itinerary].permit(:location, :travel_on)
+    @itinerary.save
+    redirect_to @itinerary
+  end
 end
