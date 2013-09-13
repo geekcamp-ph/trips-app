26380dcc3415e81a9d610cda2f0b3d2b55ab49de
Katherine G. Pe <bridgeutopia@gmail.com>
2013-09-13 16:44:04 +0800

    Added sorting

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,10 +1,13 @@
 class ItinerariesController < ApplicationController
+
+  helper_method :sort_column, :sort_direction
+
   def show
     @itinerary = Itinerary.find(params[:id])
   end
 
   def index
-    @itineraries = Itinerary.all
+    @itineraries = Itinerary.order(sort_column + " " + sort_direction)
     @remaining_budget = Itinerary.remaining_budget
   end
 
@@ -42,4 +45,15 @@ class ItinerariesController < ApplicationController
     flash[:notice] = "You have successfully deleted a itinerary"
     redirect_to action: 'index'
   end
+
+ private
+
+ def sort_column
+   Itinerary.column_names.include?(params[:sort]) ? params[:sort] : "location"
+ end
+
+ def sort_direction
+   %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+ end
+
 end
