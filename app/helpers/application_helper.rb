28844135ef28abcd8bd2054ef38b1d971c32ec51
Katherine G. Pe <bridgeutopia@gmail.com>
2013-09-14 20:51:00 +0800

    Refactored sortable helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,9 +6,9 @@ module ApplicationHelper
 
   def sortable(column, title = nil)
     title ||= column.titleize
-    css_class = column == sort_column ? "current "+sort_direction : nil
+    css_class = "current "+sort_direction if column == sort_column
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+    link_to title, {sort: column, direction: direction}, {class: css_class}
   end
 
 end
